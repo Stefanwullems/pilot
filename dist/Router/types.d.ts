9a5a2aeb70a988d10fb2a656b4e09e39b62a8713
stefanwullems <stefan@exivity.com>
2019-09-04 21:02:40 +0200

    rm

--- dist/Router/types.d.ts
+++
@@ -1,5 +0,0 @@
-export declare type Get = (name: string) => string | undefined;
-export declare type Set = (name: string, value: string) => void;
-export declare type Remove = (name: string) => void;
-export declare type Redirect = (route: string) => void;
-export declare type Includes = (route: string) => boolean;
